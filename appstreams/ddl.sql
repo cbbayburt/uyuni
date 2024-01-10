e7e8eabc17a275ebb6ad17520a9fa8e271fc8f48
Can Bulut Bayburt <cbbayburt@suse.com>
2024-01-10 18:25:50 +0100

    Parse module metadata into DB with reposync

---
+++ appstreams/ddl.sql
@@ -0,0 +1,90 @@
+-- List of modules in a channel
+CREATE TABLE IF NOT EXISTS suseModule(
+	id	NUMERIC NOT NULL
+			CONSTRAINT suse_as_module_id_pk PRIMARY KEY,
+	channel_id NUMERIC NOT NULL
+			REFERENCES rhnChannel(id)
+			ON DELETE CASCADE,
+	name	VARCHAR(128) NOT NULL,
+	stream	VARCHAR(128) NOT NULL,
+	version VARCHAR(128) NOT NULL,
+	context	VARCHAR(16) NOT NULL,
+	arch	VARCHAR(16) NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_uq_as_module_nsvca
+ON suseModule(name, stream, version, context, arch);
+
+CREATE SEQUENCE IF NOT EXISTS suse_as_module_seq;
+
+-- Which packages are included in a module
+CREATE TABLE IF NOT EXISTS suseModulePackage(
+	package_id	NUMERIC NOT NULL
+				REFERENCES rhnPackage(id)
+				ON DELETE CASCADE,
+	module_id	NUMERIC NOT NULL
+				REFERENCES suseModule(id)
+				ON DELETE CASCADE
+);
+
+-- Which modules are enabled on a server
+CREATE TABLE IF NOT EXISTS suseServerModule(
+	id	NUMERIC NOT NULL
+			CONSTRAINT suse_as_servermodule_id_pk PRIMARY KEY,
+	server_id NUMERIC NOT NULL
+			REFERENCES rhnServer(id)
+			ON DELETE CASCADE,
+	name	VARCHAR(128) NOT NULL,
+	stream	VARCHAR(128) NOT NULL,
+	version VARCHAR(128) NOT NULL,
+	context	VARCHAR(16) NOT NULL,
+	arch	VARCHAR(16) NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS suse_as_servermodule_seq;
+
+CREATE OR REPLACE VIEW modulePackage AS
+SELECT
+    m.name,
+    m.stream,
+    m.version
+    pn.name as pkg_name,
+    pe.evr,
+FROM
+    suseModule m
+    JOIN suseModulePackage mp ON m.id = mp.module_id
+    JOIN rhnPackage p on mp.package_id = p.id
+    JOIN rhnPackageName pn ON p.name_id = pn.id
+    JOIN rhnPackageEvr pe ON p.evr_id = pe.id
+ORDER BY m.name, m.stream, m.version;
+
+CREATE OR REPLACE VIEW serverModularPackages AS
+SELECT
+    c.id AS channel_id,
+    c.label AS channel_label,
+    m.id AS module_id,
+    m.name,
+    m.stream,
+    m.version,
+    m.context,
+    m.arch,
+    CONCAT(rpn.name, '-', COALESCE(rpe.epoch || ':', rpe.epoch), rpe.version, '-', rpe.release) AS package,
+    s.server_id
+FROM susemodule m
+JOIN susemodulepackage p ON p.module_id = m.id
+JOIN rhnchannel c ON m.channel_id = c.id
+JOIN rhnpackage rp ON rp.id = p.package_id
+JOIN rhnpackageevr rpe ON rpe.id = rp.evr_id
+JOIN rhnpackagename rpn ON rpn.id = rp.name_id
+LEFT JOIN suseservermodule s
+    ON  m.name = s.name
+    AND m.stream = s.stream
+    AND m.version = s.version
+    AND m.context = s.context
+    AND m.arch = s.arch
+ORDER BY c.label, m.name, m.stream, m.version, m.context, m.arch, rpn.name;
+
+
+
+-- MORE:
+-- suseModuleApi
